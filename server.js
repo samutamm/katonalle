5c5cadc883a3f6bbaa2e232b65b7cc248f8c4805
samutamm <samutamm@gmail.com>
2016-01-20 20:09:08 +0100

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static('public'));
+
+var server = app.listen(app.get('port'), function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Server started at http://localhost:%s', port);
+});
+
+module.exports = app;
