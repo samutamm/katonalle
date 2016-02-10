99db7e1342e198b8500df4bb21fe68235772a07f
samutamm <samutamm@gmail.com>
2016-02-10 09:51:22 +0100

    Configuring heroku

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.set('port', (process.env.PORT || 3000));
 var server = app.listen(app.get('port'), function () {
   var host = server.address().address;
   var port = server.address().port;
+  console.log('NODE_ENV variable: ' + process.env.NODE_ENV);
   console.log('Server started at http://localhost:%s', port);
 });
 
